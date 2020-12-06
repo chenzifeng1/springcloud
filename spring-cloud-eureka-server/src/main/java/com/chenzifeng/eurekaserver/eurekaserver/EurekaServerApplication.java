e45aa3d98e0109d21a47aa51c6964cd009d7badc
chenzifeng1 <704734862@qq.com>
2020-12-06 11:59:52 +0800

    尝试搭建eureka注册中心集群，未成功

--- spring-cloud-eureka-server/src/main/java/com/chenzifeng/eurekaserver/eurekaserver/EurekaServerApplication.java
+++ spring-cloud-eureka-server/src/main/java/com/chenzifeng/eurekaserver/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+/**
+ * @author czf
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
